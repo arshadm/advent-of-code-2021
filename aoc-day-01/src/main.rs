141ba28e806b0a107a459f0062f5738b727b04bf
Arshad Mahmood <arshadm@collably.net>
2021-12-04 15:54:17 +0000

    Completed Day 1 challenge

--- aoc-day-01/src/main.rs
+++ aoc-day-01/src/main.rs
@@ -8,14 +8,35 @@ fn depth_increases(measurements: &Vec<i32>) -> usize {
         .count()
 }
 
+fn depth_increases_sliding(measurements: &Vec<i32>) -> usize {
+    let data: Vec<i32> = measurements.iter().zip(&measurements[1..]).zip(&measurements[2..])
+        .map(|triple| triple.0.0 + triple.0.1 + triple.1)
+        .collect();
+
+    return depth_increases(&data);
+}
+
 #[cfg(test)]
 mod tests {
     use std::fs;
 
-    use crate::depth_increases;
+    use crate::{depth_increases, depth_increases_sliding};
 
-    #[test]
-    fn test_depth_increases() {
+    #[test]  // Part 1
+    fn test_depth_increases_part1() {
+        let depths = read_depth_data();
+
+        assert_eq!(depth_increases(&depths), 1688);
+    }
+
+    #[test]  // Part 2
+    fn test_depth_increases_part2() {
+        let depths = read_depth_data();
+
+        assert_eq!(depth_increases_sliding(&depths), 1728);
+    }
+
+    fn read_depth_data() -> Vec<i32> {
         let contents = fs::read_to_string("./src/data.txt").unwrap();
         let lines: Vec<&str> = contents
             .split("\n")
@@ -25,7 +46,6 @@ mod tests {
             .filter(|str| str.len() > 0)
             .map(|str: &&str| str.parse::<i32>().unwrap())
             .collect();
-
-        assert_eq!(depth_increases(&depths), 1688);
+        depths
     }
 }
