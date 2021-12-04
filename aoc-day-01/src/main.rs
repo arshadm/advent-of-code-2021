7aa378dc0c515487cb13cdcea4bf21c48a0d5da2
Arshad Mahmood <arshadm@collably.net>
2021-12-04 15:29:26 +0000

    Day 1 - part 1 completed

--- aoc-day-01/src/main.rs
+++ aoc-day-01/src/main.rs
@@ -1,3 +1,31 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn depth_increases(measurements: &Vec<i32>) -> usize {
+    measurements.iter().zip(&measurements[1..])
+        .filter(|pair| pair.0 < pair.1)
+        .count()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use crate::depth_increases;
+
+    #[test]
+    fn test_depth_increases() {
+        let contents = fs::read_to_string("./src/data.txt").unwrap();
+        let lines: Vec<&str> = contents
+            .split("\n")
+            .collect();
+
+        let depths = lines.iter()
+            .filter(|str| str.len() > 0)
+            .map(|str: &&str| str.parse::<i32>().unwrap())
+            .collect();
+
+        assert_eq!(depth_increases(&depths), 1688);
+    }
+}
